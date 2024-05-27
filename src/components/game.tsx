711489654c27605d17107c09be1ccd4f9e7bc7af
Des <ajjjjok5@gmail.com>
2024-05-26 20:35:53 -0700

    [FEAT] Update stop the game after a combo (has a winner)

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -32,13 +32,42 @@ const Square: React.FC<SquareProps> = ({
     </div>
   );
 };
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 export const Game = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null);
+  // null = not winner, false = draw
 
+  const checkWinner = (boardToCheck: any) => {
+    for (const combo of WINNER_COMBOS) {
+      //Check all the winner combinations
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] && // x or o
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    return null;
+  };
   const updateBoard = (index: number) => {
-    if (board[index]) return;
+    if (board[index] || winner) return;
     // Don't update the position if it is something
+    // Or a winner
     // Update the board
     const newBoard = [...board];
     //Spread and rest operator
@@ -48,6 +77,10 @@ export const Game = () => {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
     //To update the state we call 'setTurn' and pass the new value
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }
   };
 
   return (
